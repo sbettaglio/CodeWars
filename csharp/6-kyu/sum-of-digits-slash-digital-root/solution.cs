ffb430a11981f64522aaee0f3641efd55a5dbaa8
Stefan Bettaglio <sbettaglio@gmail.com>
2020-05-21 13:22:08 -0400

    refactored solution

--- csharp/6-kyu/sum-of-digits-slash-digital-root/solution.cs
+++ csharp/6-kyu/sum-of-digits-slash-digital-root/solution.cs
@@ -4,18 +4,10 @@ public class Number
 {
   public int DigitalRoot(long n)
   {
-    var numString = n.ToString();
-    var root = 0;
-    while (numString.Length > 1)
+    while (n > 9)
     {
-      for (var i = 0; i < numString.Length; i++)
-      {
-        var converter = Char.ToString(numString[i]);
-        root = root + int.Parse(converter);
-      }
-      numString = root.ToString();
-      root = 0;
+      n = n.ToString().Select(c => Convert.ToInt32(c.ToString())).Sum();
     }
-    return numString.Length == 0 ? 0 : Convert.ToInt32(numString);
+    return (int)n;
   }
 }
