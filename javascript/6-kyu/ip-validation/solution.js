6ec95e4ad333d51d5f98be79b41e9b1c6a61ed8f
Stefan Bettaglio <sbettaglio@gmail.com>
2020-06-03 08:50:28 -0400

    soolved

--- javascript/6-kyu/ip-validation/solution.js
+++ javascript/6-kyu/ip-validation/solution.js
@@ -1,8 +1,18 @@
 function isValidIP(str) {
-  console.log(str);
-  return str.match(
-    /^(1*(?:[0-9][0-9]?|[0-2][0-5][0-5]))\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
-  )
-    ? true
-    : false;
+  let octet = str.split(".");
+  let valid = false;
+  if (octet.length !== 4) {
+    return false;
+  } else {
+    for (let i = 0; i < octet.length; i++) {
+      if (typeof (octet[i] !== Number)) {
+        return false;
+      } else if (octet[i] >= 0 && octet[i] <= 255) {
+        valid = true;
+      } else if (octet === "0") {
+        valid = true;
+      }
+    }
+    return valid;
+  }
 }
